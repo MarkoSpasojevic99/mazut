b0668bd59bbcd20db83aa2a25112d93073e942c3
Marko Spasojeivic <markospasojevic99@gmail.com>
2024-06-13 19:02:39 +0200

    fajnal tac

--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -17,11 +17,11 @@ const getDetails = (category, itemId) => {
 };
 
 const ItemDetails = ({ item, language }) => {
-  const [activeTab, setActiveTab] = useState('description'); // default tab is description
+  const [activeTab, setActiveTab] = useState(null); // No default tab selected initially
   const details = getDetails(item.category, item.id);
   if (!details) return null;
 
-  const { en, sr, ru, brewery, price, aboutBrewery, logoImageName, beerLabelImageName } = details;
+  const { brewery, price, aboutBrewery, logoImageName, beerLabelImageName } = details;
   const description = details[language];
   const breweryInfo = brewery[language];
   const aboutBreweryInfo = aboutBrewery[language];
@@ -29,10 +29,8 @@ const ItemDetails = ({ item, language }) => {
   return (
     <div className="text-center relative">
       <div className="flex flex-col items-center mb-4">
-        <img src={require(`../assets/${logoImageName}`)} alt={item.brewery} className="w-32 h-auto mb-2" />
-        <h1 className="text-2xl mb-2 text-white font-bold">{item.name[language]}</h1>
         <img src={require(`../assets/${beerLabelImageName}`)} alt={item.name[language]} className="w-48 h-48 object-cover mx-auto mb-4" />
-        <h2 className="text-xl mb-2 text-white font-bold">{breweryInfo}</h2>
+        <h1 className="text-2xl mb-2 text-white font-bold">{item.name[language]}</h1>
         <div className="text-lg text-white font-bold mb-4">{price} €</div>
         <div className="flex justify-center mb-4">
           <button
@@ -49,14 +47,16 @@ const ItemDetails = ({ item, language }) => {
           </button>
         </div>
         <div className="p-4 bg-transparent rounded-lg">
-          {activeTab === 'description' ? (
+          {activeTab === null ? (
+            <></>
+          ) : activeTab === 'description' ? (
             <>
-              <h3 className="text-lg font-bold mb-2 text-white">Opis Piva</h3>
               <div className="text-white">{description}</div>
             </>
           ) : (
             <>
-              <h3 className="text-lg font-bold mb-2 text-white">O Pivari</h3>
+              <img src={require(`../assets/${logoImageName}`)} alt={breweryInfo} className="w-32 h-auto mb-2 mx-auto" />
+              <h2 className="text-xl mb-2 text-white font-bold">{breweryInfo}</h2>
               <div className="text-white">{aboutBreweryInfo}</div>
             </>
           )}
